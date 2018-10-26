65f61e07e179c2ee777b2d6eaed0fdbf32058d7c
rsp2000 <34970483+rsp2000@users.noreply.github.com>
2018-10-26 01:22:40 -0300

    Update run_analysis.r

--- run_analysis.r
+++ run_analysis.r
@@ -1,30 +1,44 @@
 library(dplyr)
-# read the X_train and X_test text files
-X_test<-tbl_df(read.table("./test/X_test.txt",sep="",stringsAsFactors=F,header=F))
-X_train<-tbl_df(read.table("./train/X_train.txt",sep="",stringsAsFactors=F,header=F))
 
-# 1) Merge de tables
-X_merge<-merge(X_train, X_test)
+# Go to the subdir
+setwd("./UCI HAR Dataset")
 
-# 3) Uses descriptive activity names to name the activities in the data set 
-activ_labels <- tbl_df(read.table("./activity_labels.txt",sep="",stringsAsFactors=F,header=F))
-subject_train <- tbl_df(read.table("./train/subject_train.txt",sep="",stringsAsFactors=F,header=F))
-activ_train <- merge(activ_labels, subject_train, by.x='V1', by.y='V1')
-write.csv(activ_train, file = "activ_train.csv")
-subject_test <- tbl_df(read.table("./test/subject_test.txt",sep="",stringsAsFactors=F,header=F))
-activ_test <- merge(activ_labels, subject_test, by.x='V1', by.y='V1')
-write.csv(activ_test, file = "activ_test.csv")
+# 1) Merge the tables
+# read tables first
+###################
+DS_xtrain <- read.table("./train/X_train.txt", header=FALSE)
+DS_xtest <- read.table("./test/X_test.txt", header=FALSE)
+DS_ytrain <- read.table("./train/Y_train.txt", header=FALSE)
+DS_ytest <- read.table("./test/Y_test.txt", header=FALSE)
+DS_s_train <- read.table("./train/subject_train.txt", header=FALSE)
+DS_s_test <- read.table("./test/subject_test.txt", header=FALSE)
+DS_actlbl <- read.table("./activity_labels.txt" , header=FALSE)
+DS_features <- read.table("./features.txt" , header=FALSE)
 
+# Combines (by rows) the data in order to create only one data set
+##################################################################
+DS_s_all <- rbind(DS_s_train, DS_s_test)
+DS_y_all <- rbind(DS_ytrain, DS_ytest)
+DS_x_all <- rbind(DS_xtrain, DS_xtest)
 
-# 4) Appropriately labels the data set with descriptive variable names
-features<-tbl_df(read.table("./features.txt",sep="",stringsAsFactors=F,header=F))
-colnames(X_merge) <- features$V2
+# 2) Only mean and standard deviation for each measurement
+##########################################################
+DS_meanstd <- DS_features[grep("mean\\(\\)|std\\(\\)",DS_features[,2]),]
+DS_x_all <- DS_x_all[,DS_meanstd[,1]]
 
-# 2) Extracts only the measurements on the mean and standard deviation for each measurement.
-listcolnames <- colnames(X_merge[grepl ("mean\\(\\).|std\\(\\).", colnames(X_merge))])
-X_merge_stdmean <- subset (X_merge, select=c(listcolnames))
+# 3) Descriptive activity names
+################################
+colnames(DS_y_all) <- "exec_activity"
+DS_y_all$activitylabel <- factor(DS_y_all$exec_activity, labels = as.character(DS_actlbl$V2))
+DS_act_names <- DS_y_all[,-1]
 
-# 5) From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-tidy_dataset <- colMeans(X_merge_stdmean, na.rm=TRUE)
-# Write CSV in R
-write.table(tidy_dataset, file = "tidy_dataset.txt", row.name=FALSE)
+# 4) Labels the columns with descriptive names
+##############################################
+colnames(DS_x_all) <- DS_features[DS_meanstd[,1],2]
+
+# 5) Create a tidy data set
+###########################
+colnames(DS_s_all) <- "subj_name"
+DS_final <- cbind(DS_x_all, DS_act_names, DS_s_all)
+DS_final_summarized <- DS_final %>% group_by(DS_act_names, subj_name) %>% summarise_all(mean)
+write.table(DS_final_summarized, file = "./week4_tidydata.txt", col.names = TRUE, row.names = FALSE)
